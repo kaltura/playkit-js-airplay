f0b0abc6572b166ac692f71fda06849ec2b5b6ab
Dan Ziv <dan.ziv@kaltura.com>
2020-10-25 20:28:49 +0200

    chore: logs (#7)

--- src/airplay.js
+++ src/airplay.js
@@ -37,12 +37,14 @@ class AirPlay extends BasePlugin {
   }
 
   startAirplay = () => {
+    this.logger.debug('Start airplay request');
     this.player.getVideoElement().webkitShowPlaybackTargetPicker();
   };
 
   _attachListeners() {
     if (window.WebKitPlaybackTargetAvailabilityEvent) {
       this.eventManager.listenOnce(this.player, this.player.Event.SOURCE_SELECTED, () => {
+        this.logger.debug('Attach webkit listeners');
         this.eventManager.listen(this.player.getVideoElement(), 'webkitplaybacktargetavailabilitychanged', this._availabilityChangedHandler);
         this.eventManager.listen(this.player.getVideoElement(), 'webkitcurrentplaybacktargetiswirelesschanged', this._activityChangedHandler);
       });
@@ -50,6 +52,7 @@ class AirPlay extends BasePlugin {
   }
 
   _availabilityChangedHandler = (event: Event & {availability: string}) => {
+    this.logger.debug(`Availability changed to ${event.availability}`);
     switch (event.availability) {
       case 'available':
         this.player.dispatchEvent(new FakeEvent(EventType.AIRPLAY_AVAILABILITY_CHANGED, {isAvailable: true}));
@@ -62,6 +65,7 @@ class AirPlay extends BasePlugin {
 
   _activityChangedHandler = () => {
     this._isActive = !this._isActive;
+    this.logger.debug(`Activity changed to ${this._isActive.toString()}`);
     this.player.dispatchEvent(new FakeEvent(this._isActive ? EventType.AIRPLAY_STARTED : EventType.AIRPLAY_ENDED));
   };
 }
